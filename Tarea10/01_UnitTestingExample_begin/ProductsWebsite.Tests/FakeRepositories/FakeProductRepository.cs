ba85302bf84fb75fc51d0ca2f667f3aec8297ce4
José Carlos Castillo Villarino <jcarlos.castillov@gmail.com>
2020-12-27 01:40:56 +0100

    hasta punto 20

---
+++ Tarea10/01_UnitTestingExample_begin/ProductsWebsite.Tests/FakeRepositories/FakeProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ProductsWebsite.Repositories;
+using ProductsWebsite.Models;
+
+namespace ProductsWebsite.Tests.FakeRepositories
+{
+    class FakeProductRepository
+    {
+    }
+}
